0e074f7c64f8049084ca96745915c4b7c67d3732
Devam Shroff <dev.shroff12@gmail.com>
2026-01-26 14:43:52 -0500

    updated font color gymmer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <Link href="/" className="block text-center mb-6">
-      <span className="text-2xl font-bold text-green-500">GYMMER</span>
+      <span className="text-2xl font-bold text-emerald-700">GYMMER</span>
     </Link>
   );
 }
